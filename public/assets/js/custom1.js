10b1a2e076d25fd67940cf0e8f741a03003c0639
gagan9336 <deepgagan9336@gmail.com>
2023-10-12 23:55:47 +0530

    major layout changes

--- public/assets/js/custom1.js
+++ public/assets/js/custom1.js
@@ -8,6 +8,59 @@
 //       }); }, 3000)
 //   })();
 
+
+// Gets the video src from the data-src on each button
+
+var $videoSrc = "https://www.youtube.com/embed/QcTv6nGoCM4?si=lSD4NkRvPKPRMMe2";  
+
+console.log($videoSrc+" not shown");
+
+  
+  
+// when the modal is opened autoplay it  
+$('#video').on('shown.bs.modal', function (e) {
+    
+// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+$("#video1").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+})
+  
+
+
+// stop playing the youtube video when I close the modal
+$('#video').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video1").attr('src',$videoSrc); 
+}) 
+    
+    
+
+
+  
+let accordianHead = Array.from(document.querySelectorAll(".accordian_head"));
+
+accordianHead.map((item) =>
+  item.addEventListener("click", () => {
+    closeAllAccordian(item);
+  })
+);
+
+function closeAllAccordian(current_target) {
+  console.log(current_target);
+  accordianHead.map((item) => {
+    if (current_target !== item) {
+      const accordianBody = item.nextElementSibling;
+      const togglerBtn = item.firstElementChild;
+      togglerBtn.classList.remove("active");
+      accordianBody.classList.remove("active_body");
+    } else {
+      const accordianBody = current_target.nextElementSibling;
+      const togglerBtn = item.firstElementChild;
+      togglerBtn.classList.toggle("active");
+      accordianBody.classList.toggle("active_body");
+    }
+  });
+}
+
 // Get the button:
 let mybutton = document.getElementById("scroll-up");
 
